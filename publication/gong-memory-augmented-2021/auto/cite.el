00756c8032e000c3d0b999920371e5b2f84ec744
zhen <zhen@zzhang.org>
2025-02-02 16:05:11 +1030

    personal website

---
+++ publication/gong-memory-augmented-2021/auto/cite.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "cite"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "gong_memory-augmented_2021"))
+ '(or :bibtex :latex))
+
---
+++ publication/yin-highway-2024/auto/cite.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "cite"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "yin2024highway"))
+ '(or :bibtex :latex))
+
